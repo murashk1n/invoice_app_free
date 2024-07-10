62c51cb055346d594dbdc92fe45f2fbaf06b3c5b
Andrey <GoodKid88@yandex.ru>
2024-07-11 00:02:43 +0300

    add my info page

--- views/menu.py
+++ views/menu.py
@@ -6,9 +6,9 @@ from views.app_bar import AppBar
 def page_menu(page: ft.Page, params: Params, basket: Basket):
     
     btn_customer = ft.OutlinedButton(text='Customers', width=200, on_click=lambda _:page.go('/page_all_customers'))
-    btn_companies = ft.OutlinedButton(text='Companies', width=200, on_click=lambda _:page.go('/page_all_companies'))
     btn_invoices = ft.OutlinedButton(text='Invoices', width=200, on_click=lambda _:page.go('/page_all_invoices'))
     btn_services = ft.OutlinedButton(text='Services', width=200, on_click=lambda _:page.go('/page_all_services'))
+    btn_my_info = ft.OutlinedButton(text='My info', width=200, on_click=lambda _:page.go('/page_my_info'))
 
     return ft.View(
         "/page_menu",
@@ -17,10 +17,10 @@ def page_menu(page: ft.Page, params: Params, basket: Basket):
             AppBar().build(),
             Text("Menu",size=30,weight="bold"),
             ft.Row([
-              btn_customer,
-            btn_companies,
+            btn_customer,
             btn_invoices,
-            btn_services  
+            btn_services,
+            btn_my_info
             ],alignment=MainAxisAlignment.CENTER
             ),
         ],
---
+++ views/page_my_info.py
@@ -0,0 +1,63 @@
+import flet as ft
+from flet_route import Params, Basket
+from validate_email import validate_email
+from flet import * 
+from views.app_bar import AppBar
+from util.snack_bar import show_snack_bar
+
+def page_my_info(page: ft.Page, params: Params, basket: Basket):
+    
+    def save(e):
+        if validate_email(user_email.value) == True:
+          show_snack_bar(e.page, 'Saved!')
+        #   page.go('/page_menu')
+          page.update()
+        else:
+          show_snack_bar(e.page, 'Wrong email format!')  
+
+    def validate(e):
+        if all([user_email.value, user_name.value, user_company.value, user_phone.value]):
+            btn_save.disabled = False
+        else:
+            btn_save.disabled = True
+        page.update()
+
+    user_name = ft.TextField(label='Name', width=200, on_change=validate)
+    user_company = ft.TextField(label='Company', width=200, on_change=validate)
+    user_email = ft.TextField(label='Email', width=200, on_change=validate)
+    user_phone = ft.TextField(label='Phone', width=200, on_change=validate, input_filter=ft.InputFilter(
+            allow=True,
+            regex_string=r"[0-9+]",
+            replacement_string="",
+        ))
+    btn_save = ft.OutlinedButton(text='Save', width=200, on_click=save, disabled=True)
+
+    return ft.View(
+        "/page_my_info",
+        
+        controls = [
+            AppBar().build(),
+            Text(value='MY INFO', size=30),
+            ft.Row(
+              [
+                ft.Column(
+                  [
+                    user_name,
+                    user_email,
+                  ],
+                ),
+                ft.Column(
+                  [
+                    user_company,
+                    user_phone,
+                  ],
+                ),                    
+              ],
+              alignment=ft.MainAxisAlignment.CENTER
+            ),
+            btn_save,
+          ],
+        vertical_alignment=MainAxisAlignment.CENTER,
+        horizontal_alignment=CrossAxisAlignment.CENTER,
+        spacing=26
+    ) 
